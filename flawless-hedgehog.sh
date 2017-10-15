0d41036147eac04780613ca50fc9c74f6b4d9a60
Natsec <kamilp@hotmail.fr>
2017-10-15 12:26:17 +0200

    condition while OK + if OK + message de fin

--- flawless-hedgehog.sh
+++ flawless-hedgehog.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
-conf=/etc/apache2/apache2.conf
+config=/etc/apache2/apache2.conf
+backup=${config}.before_launching_flawless-hedgehog
 
 clear
-echo "[info] Making a backup of your configuration file as ???"
-cp $conf ${conf}.before_launching_flawless-hedgehog
+echo "[info] Making a backup of your configuration file as '${backup}'"
+cp $config $backup
 
-echo -e "\n##################################################" >> $conf
-echo -e "# Security directives added by flawless-hedgehog" >> $conf
-echo -e "##################################################\n" >> $conf
+echo -e "\n##################################################" >> $config
+echo -e "# Security directives added by flawless-hedgehog" >> $config
+echo -e "##################################################\n" >> $config
 
-read -p "Do you want to hide Apache version in HTTP header ? (y/n):" ans
-while [ "$ans" != 'y' -o "$ans" != 'n' ]
+read -p $'\n'"Do you want to hide Apache version in HTTP header ? (y/n):" ans
+while [ "$ans" != 'y' ] && [ "$ans" != 'n' ]
 do
 	read -p "Do you want to hide Apache version in HTTP header ? (y/n):" ans
 done
-echo "Setting Apache version display on Production mode"
-echo 'ServerTokens Prod' >> $conf
-
+if [ "$ans" == 'y' ]
+then
+	echo "Setting Apache version display on Production mode"
+	echo 'ServerTokens Prod' >> $config
+fi
 
 echo "Disabling server signature on error pages"
-echo 'ServerSignature Off' >> $conf
+echo 'ServerSignature Off' >> $config
 
 
 
@@ -33,3 +36,7 @@ echo 'ServerSignature Off' >> $conf
 #~ grep 'timezone =' $conf
 
 service apache2 reload
+
+echo "If you feel like I fucked up your server, don't panic and run :"
+echo "sudo cp $backup $config"
+echo "Regards (^~^)"
